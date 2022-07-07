4808e770a69b6ba2558edbf1f435f8fceb7f1a03
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-07 17:32:39 -0400

    0.7.5

--- RateLimits/Limiters/IRateLimiter.cs
+++ RateLimits/Limiters/IRateLimiter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace izolabella.Util.RateLimits.Limiters
+﻿namespace izolabella.Util.RateLimits.Limiters
 {
     public interface IRateLimiter
     {
