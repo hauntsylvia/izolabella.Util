0196e5e6ae70349e18493fccc5bdf8e770f943e9
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-26 09:36:38 -0400

    new

---
+++ Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace izolabella.Util
+{
+    public static class Info
+    {
+        public static string NameForPersonal => "Mocha";
+        public static string NameForPackages => "Izolabella";
+        public static string Credit => $"⊹⊱-☿ Mercury-{NameForPackages} ☿-⊰⊹";
+    }
+}
---
+++ People/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using izolabella.Storage.Objects.Structures;
+using izolabella.Util.People.Inner;
+
+namespace izolabella.Util.People
+{
+    public class Person : IDataStoreEntity
+    {
+        public Person(string DisplayName, PronounSet[] Pronouns, ulong? Id = null)
+        {
+            this.DisplayName = DisplayName;
+            this.Pronouns = Pronouns;
+            this.Id = Id ?? IdGenerator.CreateNewId();
+        }
+
+        [JsonPropertyName("DisplayName")]
+        public string DisplayName { get; }
+
+        [JsonPropertyName("Pronouns")]
+        public PronounSet[] Pronouns { get; }
+
+        [JsonPropertyName("Id")]
+        public ulong Id { get; }
+    }
+}
