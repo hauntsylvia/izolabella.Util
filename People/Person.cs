d04f1f0b6b87837309b9442c3351babbd729fd43
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-27 09:53:42 -0400

    a

--- People/Person.cs
+++ People/Person.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using izolabella.Storage.Objects.Structures;
 using izolabella.Util.People.Inner;
+using Newtonsoft.Json;
 
 namespace izolabella.Util.People
 {
     public class Person : IDataStoreEntity
     {
+        [JsonConstructor]
         public Person(string DisplayName, PronounSet[] Pronouns, ulong? Id = null)
         {
-            this.DisplayName = DisplayName;
+            this.displayName = DisplayName;
             this.Pronouns = Pronouns;
             this.Id = Id ?? IdGenerator.CreateNewId();
         }
 
-        [JsonPropertyName("DisplayName")]
-        public string DisplayName { get; }
+        private string displayName;
+        [JsonProperty("DisplayName")]
+        public string DisplayName { get => this.displayName; set => this.displayName = value.Length >= 32 ? value[..32] : value; }
 
-        [JsonPropertyName("Pronouns")]
-        public PronounSet[] Pronouns { get; }
+        [JsonProperty("Pronouns")]
+        public PronounSet[] Pronouns { get; set; }
 
-        [JsonPropertyName("Id")]
+        [JsonProperty("Id")]
         public ulong Id { get; }
     }
 }
